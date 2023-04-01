d364d812a565a049061f2d94b41c114ee0a9f1be
Shanksmate <olamipoakinniyi@gmail.com>
2023-04-02 00:19:42 +0100

    Quiz Manager

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
 
 public class QuizManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public XRPushButton trueButton;
+    public XRPushButton falseButton;
+
+    public TMP_Text scoreText;
+    public TMP_Text questionCounterText;
+    private int playerScore = 0;
+
+    private string[] questions = new string[5] { "Question 1", "Question 2", "Question 3", "Question 4", "Question 5" };
+    private int currentQuestionIndex = 0;
+
     void Start()
     {
-        
+        trueButton.onPress.AddListener(TrueClicked);
+        falseButton.onPress.AddListener(FalseClicked);
+        ShowQuestion();
     }
 
-    // Update is called once per frame
-    void Update()
+    void TrueClicked()
     {
-        
+        CheckAnswer(true);
+    }
+
+    void FalseClicked()
+    {
+        CheckAnswer(false);
+    }
+
+    void CheckAnswer(bool answer)
+    {
+        if (answer == true)
+        {
+            Debug.Log("Correct!");
+            playerScore += 10;
+            scoreText.text = "Score: " + playerScore.ToString();
+        }
+        else
+        {
+            Debug.Log("Incorrect!");
+            playerScore -= 5;
+            scoreText.text = "Score: " + playerScore.ToString();
+        }
+
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.Length)
+        {
+            ShowQuestion();
+        }
+        else
+        {
+            EndQuiz();
+        }
+    }
+
+    void ShowQuestion()
+    {
+        // Display the current question
+        Debug.Log(questions[currentQuestionIndex]);
+        questionCounterText.text = "Question " + (currentQuestionIndex + 1).ToString() + " of " + questions.Length.ToString();
+    }
+
+    void EndQuiz()
+    {
+        // Display the final score
+        Debug.Log("Final Score: " + playerScore.ToString());
     }
 }
